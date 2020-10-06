00b9d82e80220c92975574eda8f43f9d854a2181
Yunus Emre Tatar <emre.tatar@sestek.com>
2020-10-06 17:03:41 +0300

    added json config

--- src/config/interceptor.jsx
+++ src/config/interceptor.jsx
@@ -1,37 +1,31 @@
-import axios from "axios"
+import axios from 'axios';
 
-const httpAuthorizationErrorCode = 401
-const httpForbiddenErrorCode = 403
+const httpAuthorizationErrorCode = 401;
+const httpForbiddenErrorCode = 403;
 
 const axiosInterceptor = () => {
-  const requestSuccess = config => {
-    if (isAuthenticated) {
-      config.headers["Content-Type"] = "application/json; charset=UTF-8"
-      config.headers["Authorization"] = "Bearer xxx"
+  const requestSuccess = (config) => {
+    const configuration = config;
+    configuration.headers['Content-Type'] = 'application/json; charset=UTF-8';
+    configuration.headers.Authorization = 'Bearer xxx';
+    configuration.timeout = 60000;
+    return configuration;
+  };
+  const responseSuccess = (response) => response;
+
+  const responseError = (error) => {
+    const statusCode = error.response ? error.response.status : 0;
+
+    if (error.status === httpForbiddenErrorCode) {
+      return '';
+    } if (statusCode === httpAuthorizationErrorCode) {
+      window.location.href = '/login';
     }
-    config.timeout = 60000
-    return config
-  }
-  const responseSuccess = response => {
-    return response
-  }
+    return Promise.reject(error.message);
+  };
 
-  const responseError = error => {
-    let statusCode = error.response ? error.response.status : 0
+  axios.interceptors.request.use(requestSuccess);
+  axios.interceptors.response.use(responseSuccess, responseError);
+};
 
-    if (error.status == httpForbiddenErrorCode) {
-      return ""
-    } else if (statusCode == httpAuthorizationErrorCode) {
-      if (isAuthenticated) {
-        isAuthenticated = false
-        window.location.href = "/login"
-      }
-    }
-    return Promise.reject(error.message)
-  }
-
-  axios.interceptors.request.use(requestSuccess)
-  axios.interceptors.response.use(responseSuccess, responseError)
-}
-
-export default axiosInterceptor
+export default axiosInterceptor;
