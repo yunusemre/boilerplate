6758ff9ace4267267be3c7fffcf6716a6488d9df
Yunus Emre TATAR <emretatar36@gmail.com>
2020-10-06 21:45:44 +0300

    config

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,13 @@
 import 'antd/dist/antd.css';
-import './assets/scss/app.scss';
+import 'assets/scss/app.scss';
 import React from 'react';
 import {
   Route, Switch, Redirect, BrowserRouter,
 } from 'react-router-dom';
 import { hot } from 'react-hot-loader/root';
 import ReactNotification from 'react-notifications-component';
-import Layout from './components/layouts';
-import PageHome from './container/home';
+import Layout from 'components/layouts';
+import PageHome from 'container/home';
 
 const App = () => (
   <>
