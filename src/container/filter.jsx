b0d51e0235bd105f91c87a16831598155fb28670
Yunus Emre Tatar <emre.tatar@sestek.com>
2020-10-09 18:04:25 +0300

    filter

--- src/container/filter.jsx
+++ src/container/filter.jsx
@@ -1,7 +1,8 @@
 /* eslint-disable object-curly-newline */
 import React from 'react';
-import { Form, Button, Slider } from 'antd';
+import { Form, Button, Slider, Collapse, Switch, Checkbox } from 'antd';
 
+const { Panel } = Collapse;
 const marks = {
   0: '0',
   10: '10',
@@ -20,29 +21,62 @@ const Filter = () => {
   console.log('filter');
   return (
     <Form onFinish={(form) => console.log(form)} layout="vertical" name="filter">
+      <Collapse defaultActiveKey={['1']} ghost>
+        <Panel header="POPULATION CHARACTERISTICS" key="1">
+          <Form.Item>
+            <Form.Item>
+              <h6 className="mB0 bold">Commercial (%)</h6>
+              <Form.Item name="field1">
+                <Slider dots width={400} step={5} marks={marks} defaultValue={0} />
+              </Form.Item>
+            </Form.Item>
+            <Form.Item>
+              <h6 className="mB0 bold">Medicare (%)</h6>
+              <Form.Item name="field2">
+                <Slider dots width={400} step={5} marks={marks} defaultValue={0} />
+              </Form.Item>
+            </Form.Item>
+            <Form.Item>
+              <h6 className="mB0 bold">Medicaid (%)</h6>
+              <Form.Item name="field3">
+                <Slider dots width={400} step={5} marks={marks} defaultValue={0} />
+              </Form.Item>
+            </Form.Item>
+          </Form.Item>
+        </Panel>
+        <Panel header="DISEASE STAGE" key="2">
+          <Form.Item className="switch-field">
+            <small>Overall</small>
+            <Form.Item name="field1">
+              <Switch />
+            </Form.Item>
+            <small>Cirrhotic Only</small>
+          </Form.Item>
+        </Panel>
+        <Panel header="COMPARATOR" key="3">
+          <Form.Item name="field4" className="checkbox-field">
+            <Checkbox.Group>
+              <Checkbox value="No Surveillance">No Surveillance</Checkbox>
+              <Checkbox value="Ultrasound">Ultrasound</Checkbox>
+              <Checkbox value="Ultrasound + AFP">Ultrasound + AFP</Checkbox>
+            </Checkbox.Group>
+          </Form.Item>
+        </Panel>
+        <Panel header="COSTS" key="4">
+          empty
+        </Panel>
+        <Panel header="ADHERENCE SCENARIOS" key="5">
+          <Form.Item className="switch-field">
+            <small>Basecase</small>
+            <Form.Item name="field6">
+              <Switch />
+            </Form.Item>
+            <small>Perfect</small>
+          </Form.Item>
+        </Panel>
+      </Collapse>
       <Form.Item>
-        <h4 className="mB0 htitle">POPULATION CHARACTERISTICS</h4>
-        <Form.Item>
-          <h6 className="mB0 bold">Commercial (%)</h6>
-          <Form.Item name="field1">
-            <Slider dots width={400} step={5} marks={marks} defaultValue={0} />
-          </Form.Item>
-        </Form.Item>
-        <Form.Item>
-          <h6 className="mB0 bold">Medicare (%)</h6>
-          <Form.Item name="field2">
-            <Slider dots width={400} step={5} marks={marks} defaultValue={0} />
-          </Form.Item>
-        </Form.Item>
-        <Form.Item>
-          <h6 className="mB0 bold">Medicaid (%)</h6>
-          <Form.Item name="field3">
-            <Slider dots width={400} step={5} marks={marks} defaultValue={0} />
-          </Form.Item>
-        </Form.Item>
-      </Form.Item>
-      <Form.Item>
-        <Button htmlType="submit" type="primary">Calculate</Button>
+        <Button htmlType="submit" className="mB20" type="primary">Calculate</Button>
       </Form.Item>
     </Form>
   );
