301ee8c2d83da2c47a2fcfdf246f2936475c1d7a
Yunus Emre Tatar <emre.tatar@sestek.com>
2020-10-09 09:59:44 +0300

    added header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import Logo from 'assets/images/logo.png';
 
 const Header = () => (
-  <div className="header">All right reserved </div>
+  <div className="header">
+    <h2 className="bold">ONCOGUARD Simulator</h2>
+    <div className="text-right">
+      <img src={Logo} alt="Logo" width="120" />
+    </div>
+    <style jsx>
+      {`
+      .header{ 
+        display: flex;
+        justify-content: space-between;
+
+        h2{
+          margin-bottom: 0;
+          vertical-align: bottom;
+        }
+      }
+    `}
+    </style>
+  </div>
 );
 
 export default Header;
