bef74278f288ac046d370c6010b34da353a5a9dd
Tatar <tatar@MacBook-Pro-old.local>
2020-10-11 23:32:55 +0300

    form

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -2,14 +2,31 @@ import React from 'react';
 import Logo from 'assets/images/logo.png';
 
 const Header = () => (
-  <div className="header">
-    <h2 className="bold">ONCOGUARD Simulator</h2>
-    <div className="text-right">
-      <img src={Logo} alt="Logo" width="120" />
+  <>
+    <div className="header-user">
+      <p className="content">Hello, Tatar</p>
+    </div>
+    <div className="header">
+      <h2 className="bold">ONCOGUARD Simulator</h2>
+      <div className="text-right">
+        <img src={Logo} alt="Logo" width="120" />
+      </div>
     </div>
     <style jsx>
       {`
-      .header{ 
+        .header-user{
+          background: #000;
+          height: 30px;
+          color: white;
+          text-align: right;
+
+          .content{
+            max-width: 1360px;
+            width: 100%;
+            margin: o auto;
+          }
+        }
+      .header{
         display: flex;
         justify-content: space-between;
 
@@ -20,7 +37,7 @@ const Header = () => (
       }
     `}
     </style>
-  </div>
+  </>
 );
 
 export default Header;
