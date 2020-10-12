7de1aaaccaf5c8929c0a99810c6c7e0f9351286c
Tatar <tatar@MacBook-Pro-old.local>
2020-10-12 09:16:23 +0300

    fixed input

--- src/container/filter.jsx
+++ src/container/filter.jsx
@@ -20,25 +20,37 @@ const marks = {
 const Filter = () => {
   console.log('filter');
   return (
-    <Form onFinish={(form) => console.log(form)} layout="inline" name="filter">
+    <Form
+      onFinish={(form) => console.log(form)}
+      initialValues={{
+        commercial: 30,
+        medicare: 30,
+        medicaid: 40,
+        comparator: ['No Surveillance'],
+        disease: false,
+        'costs-oncoguard': 1000,
+        'costs-resonance': 100,
+        'costs-ultrasound': 1000,
+        adherence: false }}
+      layout="inline" name="filter">
       <Collapse defaultActiveKey={['1', '2']} ghost>
         <Panel header="POPULATION CHARACTERISTICS" key="1">
           <Form.Item>
             <Form.Item>
               <h6 className="mB0 bold">Commercial (%)</h6>
-              <Form.Item name="field1">
+              <Form.Item name="commercial">
                 <Slider dots step={5} marks={marks} defaultValue={0} />
               </Form.Item>
             </Form.Item>
             <Form.Item>
               <h6 className="mB0 bold">Medicare (%)</h6>
-              <Form.Item name="field2">
+              <Form.Item name="medicare">
                 <Slider dots step={5} marks={marks} defaultValue={0} />
               </Form.Item>
             </Form.Item>
             <Form.Item>
               <h6 className="mB0 bold">Medicaid (%)</h6>
-              <Form.Item name="field3">
+              <Form.Item name="medicaid">
                 <Slider dots step={5} marks={marks} defaultValue={0} />
               </Form.Item>
             </Form.Item>
@@ -47,14 +59,14 @@ const Filter = () => {
         <Panel header="DISEASE STAGE" key="2">
           <Form.Item className="switch-field">
             <small>Overall</small>
-            <Form.Item name="field1">
+            <Form.Item name="disease">
               <Switch />
             </Form.Item>
             <small>Cirrhotic Only</small>
           </Form.Item>
         </Panel>
         <Panel header="COMPARATOR" key="3">
-          <Form.Item name="field4" className="checkbox-field">
+          <Form.Item name="comparator" className="checkbox-field">
             <Checkbox.Group>
               <Checkbox value="No Surveillance">No Surveillance</Checkbox>
               <Checkbox value="Ultrasound">Ultrasound</Checkbox>
@@ -63,21 +75,18 @@ const Filter = () => {
           </Form.Item>
         </Panel>
         <Panel header="COSTS" key="4">
-          <Form.Item name="field7" label="OncoGuard" className="input-field mB10">
+          <Form.Item name="costs-oncoguard" label="OncoGuard" className="input-field mB10">
             <InputNumber
-              defaultValue={1000}
               formatter={(value) => `$ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
               parser={(value) => value.replace(/\$\s?|(,*)/g, '')} />
           </Form.Item>
-          <Form.Item name="field8" label="M. Resonance Imaging" className="input-field mB10">
+          <Form.Item name="costs-resonance" label="M. Resonance Imaging" className="input-field mB10">
             <InputNumber
-              defaultValue={1000}
               formatter={(value) => `$ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
               parser={(value) => value.replace(/\$\s?|(,*)/g, '')} />
           </Form.Item>
-          <Form.Item name="field9" label="Ultrasound" className="input-field mB10">
+          <Form.Item name="costs-ultrasound" label="Ultrasound" className="input-field mB10">
             <InputNumber
-              defaultValue={1000}
               formatter={(value) => `$ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
               parser={(value) => value.replace(/\$\s?|(,*)/g, '')} />
           </Form.Item>
@@ -85,7 +94,7 @@ const Filter = () => {
         <Panel header="ADHERENCE SCENARIOS" key="5">
           <Form.Item className="switch-field">
             <small>Basecase</small>
-            <Form.Item name="field6">
+            <Form.Item name="adherence">
               <Switch />
             </Form.Item>
             <small>Perfect</small>
